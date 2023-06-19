d6a2c85d26627ab4bebc7a51dfe06bd70b1d810e
JeffErasLindao <jeras@espol.edu.ec>
2023-06-19 04:09:25 -0500

    Se actualizaron secciones y calculos

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -1,24 +1,39 @@
-const funcionPredeterminada = () =>{
+function funcionPredeterminada(){
 
-    const formulario = document.getElementById('formulario');
+        valorPorHora = parseInt(document.getElementById("valorPorHora").value);
+        interfazHoras = parseInt(document.getElementById("interfazHoras").value);
+        estructuraHoras = parseInt(document.getElementById("estructuraHoras").value);
+        estilizacionHoras = parseInt(document.getElementById("estilizacionHoras").value);
+        logicaHoras = parseInt(document.getElementById("logicaHoras").value);
 
-    formulario.addEventListener('submit',(evento)=>{
+        valorTotal = valorPorHora * (interfazHoras + estructuraHoras + estilizacionHoras + logicaHoras);
+
+        const valor1 = document.getElementById("valorTotal");
+        const valorNuevo = valor1.value;
+        valor1.value = valorTotal;
+}
+
+/*const funcionPredeterminada = () => {
+
+    
+    document.getElementById("formulario").addEventListener("submit", (evento) => {
+
 
         evento.preventDefault();
+        
 
-        const valorPorHora = document.getElementById('valorPorHora').value;
-        const interfazHoras = document.getElementById('interfazHoras').value;
-        const estructuraHoras = document.getElementById('estructuraHoras').value;
+        const valorPorHora = parseInt(document.getElementById("valorPorHora").value);
+        const interfazHoras = parseInt(document.getElementById("interfazHoras").value);
+        const estructuraHoras = parseInt(document.getElementById("estructuraHoras").value);
 
-        const resultado = parseInt(valorPorHora*(parseInt(interfazHoras)+parseInt(estructuraHoras)));
+        const resultado = valorPorHora*(interfazHoras+estructuraHoras);
 
-        document.getElementById('estructuraHoras').textContent = resultado;
+        document.getElementById("valorTotal").textContent = resultado;
+
+    });
+        
+
+};*/
 
 
-    })
-
-
-};
-
-
-funcionPredeterminada();
+//funcionPredeterminada();
